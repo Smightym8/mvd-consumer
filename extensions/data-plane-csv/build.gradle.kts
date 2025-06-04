0a742706d3a00e304eee258347277dca02311f86
Michael Spiegel <70846025+Smightym8@users.noreply.github.com>
2025-06-04 16:22:22 +0200

    Remove runtime-embedded and dataplane-csv

--- extensions/data-plane-csv/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    `java-library`
-}
-
-dependencies {
-    implementation(libs.edc.boot)
-    implementation(libs.apache.commons.csv)
-}
--- extensions/data-plane-csv/src/main/java/at/fhv/edc/demo/CsvDataSourceAdapter.java
+++
@@ -1,8 +0,0 @@
-package at.fhv.edc.demo;
-
-import java.util.List;
-import java.util.Map;
-
-public interface CsvDataSourceAdapter {
-    List<Map<String, String>> readData();
-}
